a852b91064a2f1bd6009800d74128c2dfe20a478
Juan-J0rge <juancontatrabalho@gmail.com>
2026-06-23 21:53:35 -0300

    Testes realizados

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,18 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve inserir dois comentarios', () => {
+        render(<PostComment />);
+        const input = screen.getByTestId('comment-input');
+        const button = screen.getByTestId('comment-button');
+
+        //insere o primeiro comentario
+        fireEvent.change(input, {target: {value: 'Primeiro comentário'}});
+        fireEvent.click(button);
+
+        //Isere o segundo comentario
+        fireEvent.change(input, {target: {value: 'Segundo comentário'}});
+        fireEvent.click(button);
+    })
 });
