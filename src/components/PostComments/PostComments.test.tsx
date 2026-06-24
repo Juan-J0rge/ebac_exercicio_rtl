fc306ef4c1ae329f6d7bfa3e4a4a5d9369c38248
Juan-J0rge <juancontatrabalho@gmail.com>
2026-06-23 22:00:59 -0300

    Testes realizados concertado

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,24 +1,30 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
+
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
 
-    it('Deve inserir dois comentarios', () => {
+    it('Deve inserir dois comentários', () => {
         render(<PostComment />);
+
         const input = screen.getByTestId('comment-input');
         const button = screen.getByTestId('comment-button');
 
-        //insere o primeiro comentario
-        fireEvent.change(input, {target: {value: 'Primeiro comentário'}});
+        // Insere o primeiro comentário
+        fireEvent.change(input, { target: { value: 'Primeiro comentário' } });
         fireEvent.click(button);
 
-        //Isere o segundo comentario
-        fireEvent.change(input, {target: {value: 'Segundo comentário'}});
+        // Insere o segundo comentário
+        fireEvent.change(input, { target: { value: 'Segundo comentário' } });
         fireEvent.click(button);
-    })
+
+        // Verifica se os dois aparecem na tela
+        expect(screen.getByText('Primeiro comentário')).toBeInTheDocument();
+        expect(screen.getByText('Segundo comentário')).toBeInTheDocument();
+    });
+
 });
